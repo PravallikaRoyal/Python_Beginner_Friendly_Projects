ba4303263c01f5225a31ed457d211de058059956
PravallikaRoyal <pravallikaroyal2004@gmail.com>
2025-10-29 16:21:43 +0530

    code for number guessing game is successfully executed

---
+++ 7_Number_Guessing_Game/number_guessing_game.py
@@ -0,0 +1,44 @@
+import art 
+logo = art.logo
+from random import randint
+easy_level_turns = 10
+hard_level_turns = 5
+
+def set_difficulty():
+    level = input("Choose a difficulty type easy or hard:")
+    if level == "easy":
+        return easy_level_turns
+    else:
+        return hard_level_turns
+    
+def check_answer(user_guess, actual_answer, turns):
+    
+    if user_guess > actual_answer:
+        print("Too High")
+        return turns-1
+    elif user_guess < actual_answer:
+        print("Too Low")
+        return turns-1
+    else:
+        print("You got it. You won the game")
+    
+def game():
+    print(logo)
+    print("Welcome to the user guessing game")
+    # choosing number from 1 to 100
+    answer = randint(1,100)
+    turns = set_difficulty()
+    user_guess = 0
+    while user_guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+        guess = int(input("Make a guess"))
+        turns = check_answer(guess,answer,turns)
+        if turns == 0:
+            print("You've run out of turns, You lose")
+            return
+        elif guess != answer:
+            print("Guess again")
+
+game()
+
+    
